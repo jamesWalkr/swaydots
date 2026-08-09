9a35f3e6e34009e30aa87a0f8ca63df8200be249
jamesWalkr <walkerjames9981@gmail.com>
2026-08-08 21:07:07 -0400

    alacritty, fish update bashrc.

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,18 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set -g theme_nerd_fonts yes
+    alias l='lsd -l'
+    alias la='lsd -lrt'
+    alias ll='lsd -lart'
+    alias grep='grep --color=auto'
+    alias gst='git status'
+    alias gp='git push'
+    alias gl='git pull'
+    alias glog='git log'
+    alias gba='git branch -a'
+    alias moi='chezmoi edit --apply $(chezmoi managed | fzf --preview "bat --color=always {}")'
+    alias fz='nvim $(fzf --preview "bat --color=always {}")'
+    set PS1 '[\u@\h \W]\$ '
+    set -gx EDITOR nvim
+    
+end
